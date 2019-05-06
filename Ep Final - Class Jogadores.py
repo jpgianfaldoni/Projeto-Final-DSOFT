8256f0be5a43edf102b5639c38901cd30dccbca6
gianlucalg <49541830+gianlucalg@users.noreply.github.com>
2019-05-06 16:24:37 -0300

    Add files via upload
    Classe inicial dos jogadores titulares do São Paulo, Santos, Palmeiras e Corinthians.

---
+++ Ep Final - Class Jogadores.py
@@ -0,0 +1,316 @@
+#Classe dos jogadores de (SÃ£o Paulo, Santos, Palmeiras e Corinthians):
+
+class Jogadores:
+    nome = " "
+    ataque = " "
+    defesa = " "
+    overall = " "
+    
+   # def mostra_stats (stats)
+        
+   
+    
+#Jogadores do SÃ£o Paulo:
+#GOL:
+Tiago_Volpi = Jogadores()
+Tiago_Volpi.nome = "Tiago Volpi"
+Tiago_Volpi.ataque = 10
+Tiago_Volpi.defesa = 75
+Tiago_Volpi.overall = (Tiago_Volpi.ataque + Tiago_Volpi.defesa)/2
+#LAT DIR:
+Hudson = Jogadores()
+Hudson.nome = "Hudson"
+Hudson.ataque = 60
+Hudson.defesa = 60
+Hudson.overall = (Hudson.ataque + Hudson.defesa)/2
+#ZAG:
+Arboleda = Jogadores()
+Arboleda.nome = "Arboleda"
+Arboleda.ataque = 20
+Arboleda.defesa = 70
+Arboleda.overall = (Arboleda.ataque + Arboleda.defesa)/2
+#ZAG:
+Bruno_Alves = Jogadores()
+Bruno_Alves.nome = "Bruno Alves"
+Bruno_Alves.ataque = 10
+Bruno_Alves.defesa = 60
+Bruno_Alves.overall = (Bruno_Alves.ataque + Bruno_Alves.defesa)/2
+#LAT ESQ:
+Reinaldo = Jogadores()
+Reinaldo.nome = "Reinaldo"
+Reinaldo.ataque = 50
+Reinaldo.defesa = 50
+Reinaldo.overall = (Reinaldo.ataque + Reinaldo.defesa)/2
+#VOL:
+Liziero = Jogadores()
+Liziero.nome = "Liziero"
+Liziero.ataque = 40
+Liziero.defesa = 55
+Liziero.overall = (Liziero.ataque + Liziero.defesa)/2
+#VOL:
+Tche_Tche = Jogadores()
+Tche_Tche.nome = "Tchê Tchê"
+Tche_Tche.ataque = 50
+Tche_Tche.defesa = 50
+Tche_Tche.overall = (Tche_Tche.ataque + Tche_Tche.defesa)/2
+#MEI:
+Everton = Jogadores()
+Everton.nome = "Everton"
+Everton.ataque = 55
+Everton.defesa = 10
+Everton.overall = (Everton.ataque + Everton.defesa)/2
+#MD:
+Anthony = Jogadores()
+Anthony.nome = "Anthony"
+Anthony.ataque = 70
+Anthony.defesa = 10
+Anthony.overall = (Anthony.ataque + Anthony.defesa)/2
+#ME:
+Toro = Jogadores()
+Toro.nome = "Toró"
+Toro.ataque = 60
+Toro.defesa = 5
+Toro.overall = (Toro.ataque + Toro.defesa)/2
+#ATA:
+Pato = Jogadores()
+Pato.nome = "Pato"
+Pato.ataque = 80
+Pato.defesa = 5
+Pato.overall = (Pato.ataque + Pato.defesa)/2
+
+#Jogadores do Santos:
+#GOL:
+Vanderlei = Jogadores()
+Vanderlei.nome = "Vanderlei"
+Vanderlei.ataque = 5
+Vanderlei.defesa = 80
+Vanderlei.overall = (Vanderlei.ataque + Vanderlei.defesa)/2
+#ZAG:
+Felipe_Aguilar = Jogadores()
+Felipe_Aguilar.nome = "Felipe Aguilar"
+Felipe_Aguilar.ataque = 10
+Felipe_Aguilar.defesa = 70
+Felipe_Aguilar.overall = (Felipe_Aguilar.ataque + Felipe_Aguilar.defesa)/2
+#ZAG:
+Lucas_Verissimo = Jogadores()
+Lucas_Verissimo.nome = "Lucas Veríssimo"
+Lucas_Verissimo.ataque = 5
+Lucas_Verissimo.defesa = 65
+Lucas_Verissimo.overall = (Lucas_Verissimo.ataque + Lucas_Verissimo.defesa)/2
+#ZAG:
+Gustavo_Henrique = Jogadores()
+Gustavo_Henrique.nome = "Gustavo Henrique"
+Gustavo_Henrique.ataque = 10
+Gustavo_Henrique.defesa = 70
+Gustavo_Henrique.overall = (Gustavo_Henrique.ataque + Gustavo_Henrique.defesa)/2
+#MEI:
+Victor_Ferraz = Jogadores()
+Victor_Ferraz.nome = "Victor Ferraz"
+Victor_Ferraz.ataque = 50
+Victor_Ferraz.defesa = 40
+Victor_Ferraz.overall = (Victor_Ferraz.ataque + Victor_Ferraz.defesa)/2
+#MEI:
+Diego_Pituca = Jogadores()
+Diego_Pituca.nome = "Diego Pituca"
+Diego_Pituca.ataque = 50
+Diego_Pituca.defesa = 35
+Diego_Pituca.overall = (Diego_Pituca.ataque + Diego_Pituca.defesa)/2
+#MEI:
+Carlos_Sanchez = Jogadores()
+Carlos_Sanchez.nome = "Carlos Sánchez"
+Carlos_Sanchez.ataque = 70
+Carlos_Sanchez.defesa = 40
+Carlos_Sanchez.overall = (Carlos_Sanchez.ataque + Carlos_Sanchez.defesa)/2
+#MEI:
+Jean_Mota = Jogadores()
+Jean_Mota.nome = "Jean Mota"
+Jean_Mota.ataque = 60
+Jean_Mota.defesa = 30
+Jean_Mota.overall = (Jean_Mota.ataque + Jean_Mota.defesa)/2
+#MEI:
+Jorge = Jogadores()
+Jorge.nome = "Jorge"
+Jorge.ataque = 50
+Jorge.defesa = 40
+Jorge.overall = (Jorge.ataque + Jorge.defesa)/2
+#ATA:
+Soteldo = Jogadores()
+Soteldo.nome = "Soteldo"
+Soteldo.ataque = 60
+Soteldo.defesa = 5
+Soteldo.overall = (Soteldo.ataque + Soteldo.defesa)/2
+#ATA:
+Rodrygo = Jogadores()
+Rodrygo.nome = "Rodrygo"
+Rodrygo.ataque = 70
+Rodrygo.defesa = 10
+Rodrygo.overall = (Rodrygo.ataque + Rodrygo.defesa)/2
+
+#Jogadores do Palmeiras:
+#GOL:
+Weverton = Jogadores()
+Weverton.nome = "Weverton"
+Weverton.ataque = 5
+Weverton.defesa = 70
+Weverton.overall = (Weverton.ataque + Weverton.defesa)/2
+#LAT DIR:
+Mayke = Jogadores()
+Mayke.nome = "Mayke"
+Mayke.ataque = 40
+Mayke.defesa = 55
+Mayke.overall = (Mayke.ataque + Mayke.defesa)/2
+#ZAG:
+Gustavo_Gomez = Jogadores()
+Gustavo_Gomez.nome = "Gustavo Goméz"
+Gustavo_Gomez.ataque = 15
+Gustavo_Gomez.defesa = 75
+Gustavo_Gomez.overall = (Gustavo_Gomez.ataque + Gustavo_Gomez.defesa)/2
+#ZAG:
+Luan = Jogadores()
+Luan.nome = "Luan"
+Luan.ataque = 10
+Luan.defesa = 70
+Luan.overall = (Luan.ataque + Luan.defesa)/2
+#LAT ESQ:
+Victor_Luis = Jogadores()
+Victor_Luis.nome = "Victor Luís"
+Victor_Luis.ataque = 40
+Victor_Luis.defesa = 45
+Victor_Luis.overall = (Victor_Luis.ataque + Victor_Luis.defesa)/2
+#VOL:
+Felipe_Melo = Jogadores()
+Felipe_Melo.nome = "Felipe Melo"
+Felipe_Melo.ataque = 35
+Felipe_Melo.defesa = 65
+Felipe_Melo.overall = (Felipe_Melo.ataque + Felipe_Melo.defesa)/2
+#VOL:
+Bruno_Henrique = Jogadores()
+Bruno_Henrique.nome = "Bruno Henrique"
+Bruno_Henrique.ataque = 50
+Bruno_Henrique.defesa = 50
+Bruno_Henrique.overall = (Bruno_Henrique.ataque + Bruno_Henrique.defesa)/2
+#MEI:
+Ze_Rafael = Jogadores()
+Ze_Rafael.nome = "Zé Rafael"
+Ze_Rafael.ataque = 55
+Ze_Rafael.defesa = 30
+Ze_Rafael.overall = (Ze_Rafael.ataque + Ze_Rafael.defesa)/2
+#MD:
+Gustavo_Scarpa = Jogadores()
+Gustavo_Scarpa.nome = "Gustavo Scarpa"
+Gustavo_Scarpa.ataque = 70
+Gustavo_Scarpa.defesa = 20
+Gustavo_Scarpa.overall = (Gustavo_Scarpa.ataque + Gustavo_Scarpa.defesa)/2
+#ME:
+Dudu = Jogadores()
+Dudu.nome = "Dudu"
+Dudu.ataque = 80
+Dudu.defesa = 20
+Dudu.overall = (Dudu.ataque + Dudu.defesa)/2
+#ATA:
+Deyverson = Jogadores()
+Deyverson.nome = "Deyverson"
+Deyverson.ataque = 65
+Deyverson.defesa = 10
+Deyverson.overall = (Deyverson.ataque + Deyverson.defesa)/2
+
+#Jogadores do Corinthians:
+#Gol:
+Cassio = Jogadores()
+Cassio.nome = "Cássio"
+Cassio.ataque = 5
+Cassio.defesa = 70
+Cassio.overall = (Cassio.ataque + Cassio.defesa)/2
+#LAT DIR:
+Fagner = Jogadores()
+Fagner.nome = "Fagner"
+Fagner.ataque = 50
+Fagner.defesa = 50
+Fagner.overall = (Fagner.ataque + Fagner.defesa)/2
+#ZAG:
+Henrique = Jogadores()
+Henrique.nome = "Henrique"
+Henrique.ataque = 10
+Henrique.defesa = 60
+Henrique.overall = (Henrique.ataque + Henrique.defesa)/2
+#ZAG:
+Manoel = Jogadores()
+Manoel.nome = "Manoel"
+Manoel.ataque = 5
+Manoel.defesa = 55
+Manoel.overall = (Manoel.ataque + Manoel.defesa)/2
+#LAT ESQ:
+Danilo_Avelar = Jogadores()
+Danilo_Avelar.nome = "Danilo Avelar"
+Danilo_Avelar.ataque = 30
+Danilo_Avelar.defesa = 30
+Danilo_Avelar.overall = (Danilo_Avelar.ataque + Danilo_Avelar.defesa)/2
+#VOL:
+Ralf = Jogadores()
+Ralf.nome = "Ralf"
+Ralf.ataque = 25
+Ralf.defesa = 55
+Ralf.overall = (Ralf.ataque + Ralf.defesa)/2
+#VOL:
+Richard = Jogadores()
+Richard.nome = "Richard"
+Richard.ataque = 30
+Richard.defesa = 40
+Richard.overall = (Richard.ataque + Richard.defesa)/2
+#MEI:
+Junior_Sornoza = Jogadores()
+Junior_Sornoza.nome = "Junior Sornoza"
+Junior_Sornoza.ataque = 55
+Junior_Sornoza.defesa = 25 
+Junior_Sornoza.overall = (Junior_Sornoza.ataque + Junior_Sornoza.defesa)/2
+#MD:
+Clayson = Jogadores()
+Clayson.nome = "Clayson"
+Clayson.ataque = 50
+Clayson.defesa = 5 
+Clayson.overall = (Clayson.ataque + Clayson.defesa)/2
+#ME:
+Pedrinho = Jogadores()
+Pedrinho.nome = "Pedrinho"
+Pedrinho.ataque = 55
+Pedrinho.defesa = 5 
+Pedrinho.overall = (Pedrinho.ataque + Pedrinho.defesa)/2
+#ATA:
+Gustavo = Jogadores()
+Gustavo.nome = "Gustagol"
+Gustavo.ataque = 55
+Gustavo.defesa = 5 
+Gustavo.overall = (Gustavo.ataque + Gustavo.defesa)/2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
