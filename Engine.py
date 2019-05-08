4113e9536b040f99aa99cd2363fc4f94cc8dbc0e
Jpgianfaldoni <jpgianfaldoni@gmail.com>
2019-05-08 14:11:20 -0300

    cria def partida

--- Engine.py
+++ Engine.py
@@ -3,25 +3,26 @@
 """
 Created on Sat May  4 19:14:42 2019
 
-@author: Joao
 """
 from Class_Jogador import Jogadores
 import random
 
-
-i = 0
-while i <= 90:
-    if (Santos.ataque) * random.randint(1,3) > Santos2.defesa * random.randint(1,3):
-        if random.randint(1,15)> 14:
-            Santos.gol += 1
-            print(i,"':","Gol do Santos!")
-            print(Santos.gol, "x", Santos2.gol)
-    if Santos2.ataque * random.randint(1,3) > Santos.defesa * random.randint(1,3):
-        if random.randint(1,15)> 14:
-            Santos2.gol += 1
-            print(i,"':","Gol do Santos 2!")
-            print(Santos.gol, "x", Santos2.gol)
-    i = i + 1
+def partida(time1, time2):
+    i = 0
+    time1.gol = 0
+    time2.gol = 0
+    while i <= 90:
+        if (time1.ataque) * random.randint(1,3) > time2.defesa * random.randint(1,3):
+            if random.randint(1,15)> 14:
+                time1.gol += 1
+                print(i,"':","Gol do time1!")
+                print(time1.gol, "x", time2.gol)
+            if tim2.ataque * random.randint(1,3) > time1.defesa * random.randint(1,3):
+                if random.randint(1,15)> 14:
+                    time2.gol += 1
+                    print(i,"':","Gol do time2!")
+                    print(time1.gol, "x", time2.gol)
+                    i = i + 1
 
 
 
