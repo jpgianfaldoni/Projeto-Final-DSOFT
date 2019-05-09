2d77a48ff1f979e28f5ef891427725b4cb50fb18
Jpgianfaldoni <jpgianfaldoni@gmail.com>
2019-05-09 09:55:34 -0300

    Terminar a lista de dicionarios

---
+++ lista_dicionarios.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 09:53:20 2019
+
+@author: Joao
+"""
+Palmeiras = {'Weverton': [5, 70, 37.5], 
+             'Mayke': [40, 55, 47.5], 
+             'Gustavo_Gomez': [15, 75, 45.0],
+             'Luan': [10, 70, 40.0], 
+             'Victor_Luis': [40, 45, 42.5],
+             'Felipe_Melo': [35, 65, 50.0], 
+             'Bruno_Henrique': [50, 50, 50.0], 
+             'Ze_Rafael': [55, 30, 42.5], 
+             'Gustavo_Scarpa': [70, 20, 45.0], 
+             'Dudu': [80, 20, 50.0], 
+             'Deyverson': [65, 10, 37.5]}
+
+Corinthians = {'Cassio': [5, 70, 37.5], 
+               'Fagner': [50, 50, 50.0], 
+               'Henrique': [10, 60, 35.0],
+               'Manoel': [5, 55, 30.0], 
+               'Danilo_Avelar': [30, 30, 30.0], 
+               'Ralf': [25, 55, 40.0], 
+               'Richard': [30, 40, 35.0], 
+               'Junior_Sornoza': [55, 25, 40.0], 
+               'Clayson': [50, 5, 27.5], 
+               'Pedrinho': [55, 5, 30.0], 
+               'Gustavo': [55, 5, 30.0]}
+
+
+
+lista_jogadores = [{"nome" : 'Tiago_Volpi',
+    "ataque" : 10  ,  
+    "defesa" : 75,
+    "overall" : 42.5,
+  },
+  {"nome" : 'Hudson',
+    "ataque": 60,
+    "defesa" : 60,
+    "overall": 60,
+  },
+  {"nome" : 'Arboleda',
+    "ataque": 20,
+    "defesa": 70,
+    "overall":  45,
+  },
+  {"nome" : 'Bruno_Alves',
+    "ataque": 10,
+    "defesa": 60,
+    "overall": 35,
+  },
+  {"nome" : 'Reinaldo',
+    "ataque": 50,
+    "defesa": 50,
+    "overall": 50,
+  },
+  {"nome" : "Liziero",
+    "ataque": 40,
+    "defesa":55,
+    "overall": 47.5,
+    
+  },
+      {"nome" : "Tche Tche",
+    "ataque": 50,
+    "defesa":50,
+    "overall": 50,
+    
+  } ,   
+      {"nome" : "Everton",
+    "ataque": 55,
+    "defesa":10,
+    "overall": 32.5,
+    
+  },
+    {"nome" : "Anthony",
+    "ataque": 70,
+    "defesa": 10,
+    "overall": 40,
+    
+  },
+    {"nome" : "Toro",
+    "ataque": 60,
+    "defesa":5,
+    "overall": 32.5,
+    
+  },
+    {"nome" : "Pato",
+    "ataque": 80,
+    "defesa":5,
+    "overall": 42.5,
+    
+  },
+    #########SANTOS
+    {"nome" : "Vanderlei",
+    "ataque": 5,
+    "defesa":80,
+    "overall": 42.5,
+    
+  },
+    {"nome" : "Felipe Aguilar",
+    "ataque": 10,
+    "defesa":70,
+    "overall": 40,
+    
+  },
+     {"nome" : "Lucas Verissimo",
+    "ataque": 5,
+    "defesa":65,
+    "overall": 35,
+    
+  },
+      {"nome" : "Gustavo Henrique",
+    "ataque": 10,
+    "defesa":70,
+    "overall": 40,
+    
+  },
+       {"nome" : "Victor Ferraz",
+    "ataque": 50,
+    "defesa":40,
+    "overall": 45,
+    
+  },
+        {"nome" : "Diego Pituca",
+    "ataque": 50,
+    "defesa":35,
+    "overall": 42.5,
+    
+  },
+         {"nome" : "Carlos Sanchez",
+    "ataque": 70,
+    "defesa":40,
+    "overall": 55,
+    
+  },
+          {"nome" : "Jean Mota",
+    "ataque": 60,
+    "defesa":30,
+    "overall": 45,
+    
+  },
+           {"nome" : "Jorge",
+    "ataque": 50,
+    "defesa":40,
+    "overall": 45,
+    
+  },
+            {"nome" : "Soteldo",
+    "ataque": 60,
+    "defesa":5,
+    "overall": 32.5,
+    
+  },
+             {"nome" : "Rodrygo",
+    "ataque": 70,
+    "defesa":10,
+    "overall": 40,
+    
+  }
+    ]
