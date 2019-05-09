55c3b50af27fcea338a7c6a912dfb03bbe78c740
Jpgianfaldoni <jpgianfaldoni@gmail.com>
2019-05-09 16:01:25 -0300

    Corrige Engine (Funcionando)

--- Engine.py
+++ Engine.py
@@ -4,29 +4,41 @@
 Created on Sat May  4 19:14:42 2019
 
 """
-from Class_Jogador import Jogadores
+from Class_Jogador import Jogador
 from Class_Time import Time
 import random
 
+
+
+jogadores = []
+for jogador in lista_jogadores:
+    novo_jogador = Jogador(jogador["nome"], jogador["ataque"],jogador["defesa"], jogador["overall"])
+    jogadores.append(novo_jogador)
+    
+    
+SaoPaulo = Time(jogadores[0:11], "Sao Paulo")
+Santos = Time(jogadores[11:22], "Santos")
+print((SaoPaulo.calcula_ataque()))
+print((Santos.calcula_ataque()))
 
 def partida(time1, time2):
     i = 0
     time1.gol = 0
     time2.gol = 0
     while i <= 90:
-        if (time1.ataque) * random.randint(1,3) > time2.defesa * random.randint(1,3):
+        if (time1.calcula_ataque()) * random.randint(1,3) > time2.calcula_defesa() * random.randint(1,3):
             if random.randint(1,15)> 14:
                 time1.gol += 1
-                print(i,"':","Gol do time1!")
+                print(i,"':","Gol do", time1.nome,"!")
                 print(time1.gol, "x", time2.gol)
-            if time2.ataque * random.randint(1,3) > time1.defesa * random.randint(1,3):
+            if time2.calcula_ataque() * random.randint(1,3) > time1.calcula_defesa() * random.randint(1,3):
                 if random.randint(1,15)> 14:
                     time2.gol += 1
-                    print(i,"':","Gol do time2!")
+                    print(i,"':","Gol do", time2.nome,"!")
                     print(time1.gol, "x", time2.gol)
         i = i + 1
 
-partida(Corinthians, Santos)
+partida(SaoPaulo, Santos)
 
 
 
