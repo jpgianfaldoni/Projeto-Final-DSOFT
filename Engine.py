6a82ecb7dfb209531695237756c12aaaf82c2449
Jpgianfaldoni <jpgianfaldoni@gmail.com>
2019-05-09 16:46:00 -0300

    adiciona lista de dicionarios

--- Engine.py
+++ Engine.py
@@ -8,7 +8,134 @@ from Class_Jogador import Jogador
 from Class_Time import Time
 import random
 
-
+lista_jogadores = [{"nome" : 'Tiago_Volpi',
+    "ataque" : 10  ,  
+    "defesa" : 75,
+    "overall" : 42.5,
+  },
+  {"nome" : 'Hudson',
+    "ataque": 60,
+    "defesa" : 60,
+    "overall": 60,
+  },
+  {"nome" : 'Arboleda',
+    "ataque": 20,
+    "defesa": 70,
+    "overall":  45,
+  },
+  {"nome" : 'Bruno_Alves',
+    "ataque": 10,
+    "defesa": 60,
+    "overall": 35,
+  },
+  {"nome" : 'Reinaldo',
+    "ataque": 50,
+    "defesa": 50,
+    "overall": 50,
+  },
+  {"nome" : "Liziero",
+    "ataque": 40,
+    "defesa":55,
+    "overall": 47.5,
+    
+  },
+      {"nome" : "Tche Tche",
+    "ataque": 50,
+    "defesa":50,
+    "overall": 50,
+    
+  } ,   
+      {"nome" : "Everton",
+    "ataque": 55,
+    "defesa":10,
+    "overall": 32.5,
+    
+  },
+    {"nome" : "Anthony",
+    "ataque": 70,
+    "defesa": 10,
+    "overall": 40,
+    
+  },
+    {"nome" : "Toro",
+    "ataque": 60,
+    "defesa":5,
+    "overall": 32.5,
+    
+  },
+    {"nome" : "Pato",
+    "ataque": 80,
+    "defesa":5,
+    "overall": 42.5,
+    
+  },
+    #########SANTOS
+    {"nome" : "Vanderlei",
+    "ataque": 5,
+    "defesa":80,
+    "overall": 42.5,
+    
+  },
+    {"nome" : "Felipe Aguilar",
+    "ataque": 10,
+    "defesa":70,
+    "overall": 40,
+    
+  },
+     {"nome" : "Lucas Verissimo",
+    "ataque": 5,
+    "defesa":65,
+    "overall": 35,
+    
+  },
+      {"nome" : "Gustavo Henrique",
+    "ataque": 10,
+    "defesa":70,
+    "overall": 40,
+    
+  },
+       {"nome" : "Victor Ferraz",
+    "ataque": 50,
+    "defesa":40,
+    "overall": 45,
+    
+  },
+        {"nome" : "Diego Pituca",
+    "ataque": 50,
+    "defesa":35,
+    "overall": 42.5,
+    
+  },
+         {"nome" : "Carlos Sanchez",
+    "ataque": 70,
+    "defesa":40,
+    "overall": 55,
+    
+  },
+          {"nome" : "Jean Mota",
+    "ataque": 60,
+    "defesa":30,
+    "overall": 45,
+    
+  },
+           {"nome" : "Jorge",
+    "ataque": 50,
+    "defesa":40,
+    "overall": 45,
+    
+  },
+            {"nome" : "Soteldo",
+    "ataque": 60,
+    "defesa":5,
+    "overall": 32.5,
+    
+  },
+             {"nome" : "Rodrygo",
+    "ataque": 70,
+    "defesa":10,
+    "overall": 40,
+    
+  }]
 
 jogadores = []
 for jogador in lista_jogadores:
