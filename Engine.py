98159f16ed53480b1dd2c1abbf673f28c3453ed5
pbcarani <49569772+pbcarani@users.noreply.github.com>
2019-05-21 19:36:06 -0300

    cod auto jogadores

--- Engine.py
+++ Engine.py
@@ -102,7 +102,7 @@ BLUE4=(  100,  0, 255)
 GREEN = (  0, 255,   0)
 RED =   (255,   0,   0)  
 LEFT=1
-time=["SP", "COR", "PAL", "SAN"]
+time=[SaoPaulo, Corinthians, Palmeiras, Santos]
 x=random.randint(0,3)
 
 def minuto(contador):
@@ -153,22 +153,22 @@ def tela1():
     text = font.render('TIME', True, WHITE)    
     screen.blit(text, [500, 50])
     if x == 0:
-        text = font.render(time[0], True, BLACK)    
+        text = font.render(str(time[0].nome), True, BLACK)    
         screen.blit(text, [500, 120])
         pygame.display.flip()
     
     if x==1:
-        text = font.render(time[1], True, BLACK)    
+        text = font.render(str(time[1].nome), True, BLACK)    
         screen.blit(text, [500, 120])
         pygame.display.flip()     
         
     if x==2:
-        text = font.render(time[2], True, BLACK)    
+        text = font.render(str(time[2].nome), True, BLACK)    
         screen.blit(text, [500, 120])
         pygame.display.flip()  
         
     if x==3: 
-        text = font.render(time[3], True, BLACK)    
+        text = font.render(str(time[3].nome), True, BLACK)    
         screen.blit(text, [500, 120])
         pygame.display.flip()
     pygame.display.flip()
@@ -207,183 +207,183 @@ def tela2():
     screen.blit(text, [420, 30])
     pygame.display.flip()    
     
-    
-    #NOMES
+
+
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[0].nome), True, WHITE)
+    text = font.render(str(time[x].jogadores[0].nome), True, WHITE)
     screen.blit(text, [10, 80])
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[1].nome), True, WHITE)
+    text = font.render(str(time[x].jogadores[1].nome), True, WHITE)
     screen.blit(text, [10, 100])
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[2].nome), True, WHITE)
+    text = font.render(str(time[x].jogadores[2].nome), True, WHITE)
     screen.blit(text, [10, 120])
-    
+         
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[3].nome), True, WHITE)
+    text = font.render(str(time[x].jogadores[3].nome), True, WHITE)
     screen.blit(text, [10, 140])
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[4].nome), True, WHITE)
+    text = font.render(str(time[x].jogadores[4].nome), True, WHITE)
     screen.blit(text, [10, 160])
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[5].nome), True, WHITE)
+    text = font.render(str(time[x].jogadores[5].nome), True, WHITE)
     screen.blit(text, [10, 180])
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[6].nome), True, WHITE)
+    text = font.render(str(time[x].jogadores[6].nome), True, WHITE)
     screen.blit(text, [10, 200])
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[7].nome), True, WHITE)
+    text = font.render(str(time[x].jogadores[7].nome), True, WHITE)
     screen.blit(text, [10, 220])
     
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[8].nome), True, WHITE)
+    text = font.render(str(time[x].jogadores[8].nome), True, WHITE)
     screen.blit(text, [10, 240])
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[9].nome), True, WHITE)
+    text = font.render(str(time[x].jogadores[9].nome), True, WHITE)
     screen.blit(text, [10, 260])    
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[10].nome), True, WHITE)
+    text = font.render(str(time[x].jogadores[10].nome), True, WHITE)
     screen.blit(text, [10, 280])
-    #ATQ
+        #ATQ
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[0].ataque), True, WHITE)
+    text = font.render(str(time[x].jogadores[0].ataque), True, WHITE)
+    
     screen.blit(text, [280, 80])
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[1].ataque), True, WHITE)
+    text = font.render(str(time[x].jogadores[1].ataque), True, WHITE)
     screen.blit(text, [280, 100])
-    
-    
+        
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[2].ataque), True, WHITE)
+    text = font.render(str(time[x].jogadores[2].ataque), True, WHITE)
     screen.blit(text, [280, 120])
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[3].ataque), True, WHITE)
+    text = font.render(str(time[x].jogadores[3].ataque), True, WHITE)
     screen.blit(text, [280, 140])
-
+    
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[4].ataque), True, WHITE)
+    text = font.render(str(time[x].jogadores[4].ataque), True, WHITE)
     screen.blit(text, [280, 160])
-
+    
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[5].ataque), True, WHITE)
+    text = font.render(str(time[x].jogadores[5].ataque), True, WHITE)
     screen.blit(text, [280, 180])    
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[6].ataque), True, WHITE)
+    text = font.render(str(time[x].jogadores[6].ataque), True, WHITE)
     screen.blit(text, [280, 200])    
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[7].ataque), True, WHITE)
+    text = font.render(str(time[x].jogadores[7].ataque), True, WHITE)
     screen.blit(text, [280, 220])    
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[8].ataque), True, WHITE)
+    text = font.render(str(time[x].jogadores[8].ataque), True, WHITE)
     screen.blit(text, [280, 240])    
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[9].ataque), True, WHITE)
+    text = font.render(str(time[x].jogadores[9].ataque), True, WHITE)
     screen.blit(text, [280, 260])    
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[10].ataque), True, WHITE)
+    text = font.render(str(time[x].jogadores[10].ataque), True, WHITE)
     screen.blit(text, [280, 280])    
+        
     
-
-    #DEF
+        #DEF
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[0].defesa), True, WHITE)
+    text = font.render(str(time[x].jogadores[0].defesa), True, WHITE)
     screen.blit(text, [350, 80])
 
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[1].defesa), True, WHITE)
+    text = font.render(str(time[x].jogadores[1].defesa), True, WHITE)
     screen.blit(text, [350, 100])
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[2].defesa), True, WHITE)
+    text = font.render(str(time[x].jogadores[2].defesa), True, WHITE)
     screen.blit(text, [350, 120])
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[3].defesa), True, WHITE)
+    text = font.render(str(time[x].jogadores[3].defesa), True, WHITE)
     screen.blit(text, [350, 140])
-    
+        
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[4].defesa), True, WHITE)
+    text = font.render(str(time[x].jogadores[4].defesa), True, WHITE)
     screen.blit(text, [350, 160])
-    
+        
     
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[5].defesa), True, WHITE)
+    text = font.render(str(time[x].jogadores[5].defesa), True, WHITE)
     screen.blit(text, [350, 180])
     
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[6].defesa), True, WHITE)
+    text = font.render(str(time[x].jogadores[6].defesa), True, WHITE)
     screen.blit(text, [350, 200])
     
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[7].defesa), True, WHITE)
+    text = font.render(str(time[x].jogadores[7].defesa), True, WHITE)
     screen.blit(text, [350, 220])
     
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[8].defesa), True, WHITE)
+    text = font.render(str(time[x].jogadores[8].defesa), True, WHITE)
     screen.blit(text, [350, 240])
     
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[9].defesa), True, WHITE)
+    text = font.render(str(time[x].jogadores[9].defesa), True, WHITE)
     screen.blit(text, [350, 260])
     
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[10].defesa), True, WHITE)
+    text = font.render(str(time[x].jogadores[10].defesa), True, WHITE)
     screen.blit(text, [350, 280])
-    
+        
     #OV
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[0].overall), True, WHITE)
+    text = font.render(str(time[x].jogadores[0].overall), True, WHITE)
     screen.blit(text, [420, 80])
     
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[1].overall), True, WHITE)
+    text = font.render(str(time[x].jogadores[1].overall), True, WHITE)
     screen.blit(text, [420, 100])
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[2].overall), True, WHITE)
+    text = font.render(str(time[x].jogadores[2].overall), True, WHITE)
     screen.blit(text, [420, 120])
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[3].overall), True, WHITE)
+    text = font.render(str(time[x].jogadores[3].overall), True, WHITE)
     screen.blit(text, [420, 140])
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[4].overall), True, WHITE)
+    text = font.render(str(time[x].jogadores[4].overall), True, WHITE)
     screen.blit(text, [420, 160])
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[5].overall), True, WHITE)
+    text = font.render(str(time[x].jogadores[5].overall), True, WHITE)
     screen.blit(text, [420, 180])
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[6].overall), True, WHITE)
+    text = font.render(str(time[x].jogadores[6].overall), True, WHITE)
     screen.blit(text, [420, 200])
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[7].overall), True, WHITE)
+    text = font.render(str(time[x].jogadores[7].overall), True, WHITE)
     screen.blit(text, [420, 220])
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[8].overall), True, WHITE)
+    text = font.render(str(time[x].jogadores[8].overall), True, WHITE)
     screen.blit(text, [420, 240])
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[9].overall), True, WHITE)
+    text = font.render(str(time[x].jogadores[9].overall), True, WHITE)
     screen.blit(text, [420, 260])
     font = pygame.font.SysFont(None, 30)
-    text = font.render(str(SaoPaulo.jogadores[10].overall), True, WHITE)
+    text = font.render(str(time[x].jogadores[10].overall), True, WHITE)
     screen.blit(text, [420, 280])
     
     pygame.display.flip()
-
-
+    
 
 def tela3(contador):
     times=[SaoPaulo, Corinthians, Santos, Palmeiras]
@@ -482,7 +482,7 @@ def tela3(contador):
     font = pygame.font.SysFont(None, 30)
     text = font.render(str(minuto_partida(rodada1[1][0], rodada1[1][1], contador)), True, GREEN)
     screen.blit(text, [245, 50])
-#    
+  
     pygame.display.flip()
     
 
