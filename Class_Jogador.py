e03463dea1b9f001f95934fb43999fa773846296
Jpgianfaldoni <jpgianfaldoni@gmail.com>
2019-06-01 15:40:48 -0300

    adiciona gols

--- Class_Jogador.py
+++ Class_Jogador.py
@@ -1,9 +1,10 @@
 class Jogador:
-    def __init__(self, nome, ataque, defesa, overall):
+    def __init__(self, nome, ataque, defesa, overall,gols):
         self.nome  = nome
         self.ataque = ataque
         self.defesa = defesa
         self.overall = overall
+        self.gols = gols
 
 
 
